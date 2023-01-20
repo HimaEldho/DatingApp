1f222e4b4ce81c1546ddbe6e6858c049cf22e0d0
Hima Eldho <himaeldho7@gmail.com>
2023-01-20 15:38:51 +0530

    End Of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,11 +3,17 @@
 // using System.Linq;
 // using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id  { get; set; }
-         public string UserName  { get; set; }
+
+        [Required]
+        public string UserName  { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
